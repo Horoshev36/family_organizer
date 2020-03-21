43dc5d25413ddccf9055382077ba9f730555c5bd
Drakonov <lazurel36@gmail.com>
2020-03-21 20:42:39 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:familyorganizer/SingInUp/ChooseInUp.dart';
+import 'package:flutter/material.dart';
+
+
+void main() => runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: ChooseInUp()));
